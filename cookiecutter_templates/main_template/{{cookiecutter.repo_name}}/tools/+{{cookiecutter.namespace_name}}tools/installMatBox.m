20f2e2f14d7ba2ae4f0d423e1e816fe8c7d7c62c
ehennestad <ehennestad@gmail.com>
2026-05-21 09:50:54 +0200

    Update installMatBox.m
    Use installer from matbox-actions repository in order to have one "ground truth" installer

--- cookiecutter_templates/main_template/{{cookiecutter.repo_name}}/tools/+{{cookiecutter.namespace_name}}tools/installMatBox.m
+++ cookiecutter_templates/main_template/{{cookiecutter.repo_name}}/tools/+{{cookiecutter.namespace_name}}tools/installMatBox.m
@@ -1,63 +1,26 @@
 function installMatBox(mode)
-% installMatBox - Install MatBox from latest release or latest commit
-
-%   Todo:
-%   - If MatBox release has been updated on remote, should reinstall.
+% installMatBox - Bootstrap MatBox using the matbox-actions installer.
 
     arguments
-        mode (1,1) string {mustBeMember(mode, ["release", "commit"])} = "release"
+        mode (1,1) string {mustBeMember(mode, ["release", "commit"])} = "commit"
     end
-    
-    if mode == "release"
-        installFromRelease()    % local function
-    elseif mode == "commit"
-        installFromCommit()     % local function
+
+    if exist("+matbox/installRequirements", "file") == 2
+        return
     end
-end
 
-function installFromRelease()
-    addonsTable = matlab.addons.installedAddons();
-    isMatchedAddon = addonsTable.Name == "MatBox";
-    
-    if ~isempty(isMatchedAddon) && any(isMatchedAddon)
-        matlab.addons.enableAddon('MatBox')
-    else
-        info = webread('https://api.github.com/repos/ehennestad/MatBox/releases/latest');
-        assetNames = {info.assets.name};
-        isMltbx = startsWith(assetNames, 'MatBox');
-    
-        mltbx_URL = info.assets(isMltbx).browser_download_url;
-        
-        % Download matbox
-        tempFilePath = websave(tempname, mltbx_URL);
-        cleanupObj = onCleanup(@(fp) delete(tempFilePath));
-        
-        % Install toolbox
-        matlab.addons.install(tempFilePath);
-    end
-end
+    sourceFile = "https://raw.githubusercontent.com/ehennestad/matbox-actions/refs/heads/main/install-matbox/installMatBox.m";
 
+    tempFolder = tempname;
+    mkdir(tempFolder)
+    cleanupObj = onCleanup(@() rmdir(tempFolder, "s"));
 
-function installFromCommit()
-    % Download latest zipped version of repo
-    url = "https://github.com/ehennestad/MatBox/archive/refs/heads/main.zip";
-    tempFilePath = websave(tempname, url);
-    cleanupObj = onCleanup(@(fp) delete(tempFilePath));
-    
-    % Unzip in temporary location
-    unzippedFiles = unzip(tempFilePath, tempdir);
-    unzippedFolder = unzippedFiles{1};
-    if endsWith(unzippedFolder, filesep)
-        unzippedFolder = unzippedFolder(1:end-1);
-    end
-    
-    % Move to installation location
-    [~, repoFolderName] = fileparts(unzippedFolder);
-    targetFolder = fullfile(userpath, "Add-Ons");
-    targetFolder = fullfile(targetFolder, repoFolderName);
-    if isfolder(targetFolder); rmdir(targetFolder, "s"); end
-    movefile(unzippedFolder, targetFolder);
+    websave(fullfile(tempFolder, "installMatBox.m"), sourceFile);
 
-    % Add to MATLAB's search path
-    addpath(genpath(targetFolder))
+    oldPath = addpath(tempFolder, "-begin");
+    pathCleanupObj = onCleanup(@() path(oldPath));
+
+    rehash()
+    installMatBox(mode)
+    rehash()
 end
